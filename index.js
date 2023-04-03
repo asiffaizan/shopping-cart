96d9a26156d79c1114e2eda60fc1ad3887b0a76f
asiffaizan <asiffaizan0077@gmail.com>
2023-04-03 12:41:59 +0600

    add functionality on price

--- index.js
+++ index.js
@@ -1,67 +1,53 @@
-//phones event handler
-//remove button event Listener
-const removeBtn = document.getElementById("removePhone");
-removeBtn.addEventListener("click",function(){
-    const phonesQuantity = document.getElementById("phonesQuantity").value;
-    const phonesQuantityNumber = parseFloat(phonesQuantity);
-    const totalQuantity = phonesQuantityNumber - 1;
-    document.getElementById("phonesQuantity").value = totalQuantity;
-
-    //update parches amount
-    const phonesPrice = document.getElementById("phonesPrice").innerText;
-    const phonesPriceNumber = parseFloat(phonesPrice);
-    const totalPrice = phonesPriceNumber*phonesQuantityNumber;
-    // document.getElementById("phonesPrice").innerText = totalPrice;
-    console.log("this is remove button : ",totalPrice);
-})
-
+//<<<<============phones event handler========>>>
 //add button event listener
 const addBtn = document.getElementById("addPhone");
 addBtn.addEventListener("click",function(){
-    const phonesQuantity = document.getElementById("phonesQuantity").value;
-    const phonesQuantityNumber = parseFloat(phonesQuantity);
-    const totalQuantity = phonesQuantityNumber + 1;
-    document.getElementById("phonesQuantity").value = totalQuantity;
-    //update parches amount
-    const phonesPrice = document.getElementById("phonesPrice").innerText;
-    const phonesPriceNumber = parseFloat(phonesPrice);
-    const totalPrice = phonesPriceNumber*totalQuantity;
-    document.getElementById("phonesPrice").innerText = totalPrice;
-    console.log("add button : ", totalPrice," phonesQuantity", phonesQuantity," totalQuantity ", totalQuantity," phonesPrice ", phonesPrice," phonesPriceNumber ", phonesPriceNumber);
+    increaseAmount("phonesQuantity","phonesPrice",1219);
+})
+//remove button event Listener
+const removeBtn = document.getElementById("removePhone");
+removeBtn.addEventListener("click",function(){
+    decreaseAmount("phonesQuantity","phonesPrice",1219);
 })
 
 
-//Casing event handler
-
+//<<<<============Casing event handler========>>>>>
 //add button event listener
 const addCasingQuantity = document.getElementById("addCasingQuantity");
 addCasingQuantity.addEventListener("click",function(){
-    const casingQuantity = document.getElementById("casingQuantity").value;
-    const casingQuantityNumber = parseFloat(casingQuantity);
-    const totalQuantity = casingQuantityNumber + 1;
-    document.getElementById("casingQuantity").value = totalQuantity;
-    //update parches amount
-    const casingPrice = document.getElementById("casingPrice").innerText;
-    const casingPriceNumber = parseFloat(casingPrice);
-    const totalPrice = casingPriceNumber*totalQuantity;
-    document.getElementById("casingPrice").innerText = totalPrice;
-    console.log("add button : ", totalPrice," casingQuantity", casingQuantity," totalQuantity ", totalQuantity," casingPrice ", casingPrice," casingPriceNumber ", casingPriceNumber);
+   increaseAmount("casingQuantity","casingPrice",59);
 })
-
-//add button event listener
+//remove button event listener
 const removeCasingQuantity = document.getElementById("removeCasingQuantity");
 removeCasingQuantity.addEventListener("click",function(){
-    const casingQuantity = document.getElementById("casingQuantity").value;
-    const casingQuantityNumber = parseFloat(casingQuantity);
-    const totalQuantity = casingQuantityNumber - 1;
-    document.getElementById("casingQuantity").value = totalQuantity;
-    //update parches amount
-    const casingPrice = document.getElementById("casingPrice").innerText;
-    const casingPriceNumber = parseFloat(casingPrice);
-    const totalPrice = casingPriceNumber*totalQuantity;
-    document.getElementById("casingPrice").innerText = totalPrice;
-    console.log("add button : ", totalPrice," casingQuantity", casingQuantity," totalQuantity ", totalQuantity," casingPrice ", casingPrice," casingPriceNumber ", casingPriceNumber);
+    decreaseAmount("casingQuantity","casingPrice",59);
 })
+
+//<<<<============Total Price========>>>>>
+
+
+
 
 
 //common function
+function increaseAmount(btnId,valueId,productPriceNumber) {
+    const productQuantity = document.getElementById(btnId).value;
+    const productNumber = parseFloat(productQuantity);
+    const totalQuantity = productNumber + 1;
+    document.getElementById(btnId).value = totalQuantity;
+    //update parches amount
+    amount(totalQuantity,productPriceNumber,valueId);
+}
+function decreaseAmount(btnId,valueId,productPriceNumber) {
+    const productQuantity = document.getElementById(btnId).value;
+    const productNumber = parseFloat(productQuantity);
+    const totalQuantity = productNumber - 1;
+    document.getElementById(btnId).value = totalQuantity;
+    // //update parches amount
+    amount(totalQuantity,productPriceNumber,valueId);
+}
+
+function amount(totalQuantity,productPriceNumber,valueId) {
+    const totalPrice = productPriceNumber*totalQuantity;
+    document.getElementById(valueId).innerText = totalPrice;
+}
